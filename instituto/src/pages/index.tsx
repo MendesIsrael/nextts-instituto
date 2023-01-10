6fb1c55dd75dc7bc77089a47af7e204d46bb8500
Israel Mendes da Silva <israel.mendes97@hotmail.com>
2023-01-09 22:46:13 -0300

    importação das imagens do home

--- instituto/src/pages/index.tsx
+++ instituto/src/pages/index.tsx
@@ -1,6 +1,19 @@
+import { HomeContainer } from '../styles/pages/home'
+
+import Image from 'next/legacy/image'
+import logoInstituto from '../assets/logos/logo-3.png'
+import logoVigor from '../assets/logos/logo-vigor-preto.png'
+import logoSeaside from '../assets/logos/logo-branca-seaside.png'
+import fotoIsrael from '../assets/fotos/israel.png'
+import fotoRafaela from '../assets/fotos/rafaela.png'
+import parceiroArkhi from '../assets/parceiros/arkhi-design.png'
+
 
 export default function Home() {
   return (
-    <h1>oi</h1>
+    <HomeContainer>
+      <h1>oi</h1>
+      <Image src={logoInstituto} width={300} height={300} />
+    </HomeContainer>
   )
 }
