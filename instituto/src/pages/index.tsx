a48db3bc3909a0d73a49a3ceac6f168908cda525
Israel Mendes da Silva <israel.mendes97@hotmail.com>
2023-01-09 23:24:11 -0300

    Implementação quem somos

--- instituto/src/pages/index.tsx
+++ instituto/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { HomeContainer } from '../styles/pages/home'
+import { ContainerMissaoVisao, HomeContainer, MissaoVisao, QuemSomos, Title } from '../styles/pages/home'
 
 import Image from 'next/legacy/image'
 import logoInstituto from '../assets/logos/logo-3.png'
@@ -12,8 +12,24 @@ import parceiroArkhi from '../assets/parceiros/arkhi-design.png'
 export default function Home() {
   return (
     <HomeContainer>
-      <h1>oi</h1>
-      <Image src={logoInstituto} width={300} height={300} />
+      <Title>
+        <Image src={logoInstituto} width={560} height={560} />
+      </Title>
+
+      <QuemSomos>
+        <h1>QUEM SOMOS</h1>
+        <p>O Instituto Mendes de Sá é uma entidade brasileira privada criada com o propósito de levantar a bandeira do esporte e da educação como ferramenta de transformação, trabalhando o desenvolvimento humano, físico e cognitivo. Com foco no corpo e mente, oferece atividades coletivas, treinos, palestras e cursos com aulas teóricas e práticas nas áreas da saúde e meio ambiente, levando - de forma ética e responsável - a população Nilopolitana a dar novos passos através de domínios técnicos e científicos. Fundado em Maio de 2022, por Israel Mendes e Rafaela de Sá, atende crianças, jovens, adultos e idosos que buscam conhecimento, bem-estar ou melhora da performance.</p>
+        <ContainerMissaoVisao>
+          <MissaoVisao>
+            <h1>Visão</h1>
+            <p>Tornar-se referência por excelência, se estabelecendo como uma organização sólida, buscando preencher a lacuna acadêmica na Baixada Fluminense referente a Cursos livres e, além disso, suprir as necessidades do público que visa desenvolver o seu potencial máximo, melhorar o condicionamento físico, prevenir e reduzir os ricos de lesões e o alívio de dores crônicas.</p>
+          </MissaoVisao>
+          <MissaoVisao>
+            <h1>Missão</h1>
+            <p>A palavra “Instituto” vem do latim <em>“Statuere”</em>, que significa “colocar em pé” ou “firmar”. Desse modo, a nossa missão é estabelecer raízes profundas no município de Nilópolis, mantendo de pé os anseios de nossos alunos, seja aproximando-os do conhecimento científico ou promovendo o alcance da qualidade de vida.</p>
+          </MissaoVisao>
+        </ContainerMissaoVisao>
+      </QuemSomos>
     </HomeContainer>
   )
 }
--- instituto/src/styles/pages/home.ts
+++ instituto/src/styles/pages/home.ts
@@ -1,5 +1,39 @@
 import { styled } from "..";
 
 export const HomeContainer = styled('main', {
-  
-})
+  h1: {
+    textAlign: 'center',
+  },
+
+  p: {
+    textAlign : 'justify',
+    padding   : '2rem',
+    textIndent: '2.5rem',
+  }
+});
+
+export const Title = styled('main', {
+  backgroundColor : '$black',
+  display         : 'flex',
+  justifyContent  : 'center',
+
+  img: {
+    objectFit: 'cover',
+  }
+});
+
+export const QuemSomos = styled('div', {
+  backgroundColor : '$gray300',
+  color           : '$white',
+  paddingTop      : '2rem',
+});
+
+export const ContainerMissaoVisao = styled('div', {
+  display : 'flex',
+  margin  : '0px, 8px',
+});
+
+export const MissaoVisao = styled('div', {
+  boxShadow : '0px 0px 2px black',
+  width     : '50%',
+});
