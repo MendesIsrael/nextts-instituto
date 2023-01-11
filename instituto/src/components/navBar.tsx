df230be9bcd7c9397d11c79869a79a87571c389b
Israel Mendes da Silva <israel.mendes97@hotmail.com>
2023-01-11 10:11:09 -0300

    começando a adicionar o link

---
+++ instituto/src/components/navBar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+export default function NavBar() {
+  return (
+    <>
+    <nav>
+      <ul>
+        <li>
+          <Link></Link>
+        </li>
+      </ul>
+    </nav>
+    </>
+  )
+}
